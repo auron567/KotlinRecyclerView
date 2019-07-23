fc287745fb7c87b16323c2c528a32d8d0649b456
auron567 <xauron567x@gmail.com>
2019-07-23 10:33:57 +0200

    Add option to switch between span sizes

--- app/src/main/java/com/example/kotlinrecyclerview/ui/all/AllFragment.kt
+++ app/src/main/java/com/example/kotlinrecyclerview/ui/all/AllFragment.kt
@@ -1,9 +1,7 @@
 package com.example.kotlinrecyclerview.ui.all
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -19,6 +17,12 @@ class AllFragment : Fragment() {
     }
 
     private val adapter = CreatureCardAdapter(CreatureStore.getCreatures().toMutableList())
+    private lateinit var layoutManager: StaggeredGridLayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_all, container, false)
@@ -27,8 +31,35 @@ class AllFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
+        layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
         creature_recycler_view.layoutManager = layoutManager
         creature_recycler_view.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_all, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_span_1 -> {
+                showListView()
+                return true
+            }
+            R.id.action_span_2 -> {
+                showGridView()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showListView() {
+        layoutManager.spanCount = 1
+    }
+
+    private fun showGridView() {
+        layoutManager.spanCount = 2
     }
 }
